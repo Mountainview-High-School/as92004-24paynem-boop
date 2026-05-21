db1b522d9f90d013f8eb5ec1f63329688f7a3641
24paynem-boop <24paynem@mountainview.school.nz>
2026-05-21 13:17:11 +1200

    Upgraded lists

--- Quiz.py
+++ Quiz.py
@@ -12,14 +12,16 @@ if age > 13:
     sys.exit("Ending program")
     # The program will only be ended if inputed age is over 13 since this is all indented
 time.sleep(.5)
+#loop needs to be implemented here.
 questions = ["Someone sends you a text that is hurtful and makes you feel bad about yourself. What should you do?", "You find out somebody has posted an embarrassing picture of you online. What should you do?", "You want to join an online gaming site. Which of the following infomation is okay for you to post on the site"]
-print(questions[0])
+qno = 0 
+print(questions[qno])
 choices = ["a. Delete the message and try to forget about it", "b. Keep the text and show an adult you trust", "c. Text the person back saying something mean to them" ]
-print(choices[0])
+print(choices[qno])
 time.sleep(.5)
-print(choices[1])
+print(choices[qno +1])
 time.sleep(.5)
-print(choices[2])
+print(choices[qno +2])
 time.sleep(1)
 print("Input : A, B or C")
 i=0
